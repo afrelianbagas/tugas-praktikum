bffa5cfe3ccbed1d7f017ee5f866ce0edba1b752
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-09-11 07:53:09 +0700

    Add files via upload

---
+++ akar_pangkat/src/main/java/com/mycompany/akar_pangkat/Akar_pangkat.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.akar_pangkat;
+
+/**
+ *
+ * @author Afrelian
+ */
+public class Akar_pangkat {
+
+   public static void main(String [] args) 
+{ 
+int a=10; int b= 2; 
+double akar_a=Math.pow(a,2); 
+double akar3_b=Math.pow(b,3); 
+double penjumlahan_keduanya=akar_a+akar_b; 
+// 
+} 
+}
