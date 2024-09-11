bfe71864605a4874431676c59a8f00f7cdd671ee
afrelianbagas <145529519+afrelianbagas@users.noreply.github.com>
2024-09-11 07:56:57 +0700

    Add files via upload

---
+++ Soal2/src/main/java/com/mycompany/soal2/Soal2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal2;
+
+/**
+ *
+ * @author Afrelian
+ */
+public class Soal2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
